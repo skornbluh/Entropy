9782ef4d5a9cbbb0ce35b1cda1f35322dda3323a
skornbluh <skornbluh@gmail.com>
2017-05-22 15:35:36 -0500

    Update Entropy.java

--- Entropy.java
+++ Entropy.java
@@ -1,79 +1,60 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
 
 public class Entropy {
-	private static String data;
-	private static HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
-	
-	/**
-	 * gets data from file and stores in data
-	 */
-	public static void getData() {
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader("test_entropy_02.txt"));
-			StringBuilder sb = new StringBuilder();
-			String line = "";
-			do {
-				line = br.readLine();
-				if (line != null) {
-					sb.append(line);
-				}
-			} while (line != null);
-			data = sb.toString();
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(0);
-		} 
-	}
-	
-	public static double computeEntropy() {
-		int n = data.length();
 
-		for (int i = 0; i < n; i++) { // count occurrences of letters
-			char ch = data.charAt(i);
-			countOccurrences(ch);
-		}
-		
-		double entropy = 0.0;
-		for (Map.Entry<Character, Integer> item : charCount.entrySet()) { // iterate over character count hashmap
-			entropy += item.getValue();
-		}
-		return entropy/n;
-	}
-	
-	private static int countOccurrences(char ch) {
-		if (charCount.containsKey(ch)) {
-			return -1;
-		}
-		int count = 0;
+	private static String data;
+
+	private static double entropy = 0;
+
+
+	public static void countOccurrences() {
+		char[] uniqueCharacters = new char[data.length()];
 		for (int i = 0; i < data.length(); i++) {
-			if (data.charAt(i) == ch) {
-				count++;
+			char letter = data.charAt(i);
+
+			String substring = data.substring(0, i);
+
+			if (substring.indexOf(letter) == -1) {
+				uniqueCharacters[i] = letter;
+				System.out.println(uniqueCharacters);
 			}
 		}
-		charCount.put((char)ch, (int)count); // store the character and its count in the hashmap
-		return count;
-	}
 
-	/**
-	 * run the program
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		getData();
-		System.out.println("Entropy for '" + data + "' is " + computeEntropy());
-		
-		for (Map.Entry<Character, Integer> item : charCount.entrySet()) { // iterate over character count hashmap
-			System.out.println("key = " + item.getKey() + ", value = " + item.getValue());
+		for (int j = 0; j < uniqueCharacters.length; j++) {
+			int counter = 0;
+			for (int k = 0; k < data.length(); k++) {
+				if (uniqueCharacters[j] == data.charAt(k)) {
+					counter++;
+				}
+			}
+			System.out.print(uniqueCharacters[j]);
+			System.out.println(counter);
+
+			computeEntropy(counter);
+
 		}
-		
-
 	}
 
+	public static void computeEntropy(double counter) {
+
+		if (counter != 0) {
+			System.out.println("Calling compute entryopy for " + counter);
+			double probability = counter / data.length();
+			System.out.println("letter probability is " + probability);
+
+			double letterEntropy = probability * (Math.log(probability) / Math.log(2));
+			entropy += letterEntropy;
+		}
+	}
+
+	public static void main(String[] args) {
+		data = args[0];
+
+		countOccurrences();
+
+		System.out.println(data);
+		System.out.println("entropy = " + entropy * -1);
+
+	}
 }
